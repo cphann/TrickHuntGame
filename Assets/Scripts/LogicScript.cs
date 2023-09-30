1de2f1e9250988547951221b984fc45f3ea2392d
Emily Chung <emchung214@gmail.com>
2023-09-30 12:31:36 -0400

    reupload

--- Assets/Scripts/LogicScript.cs
+++ Assets/Scripts/LogicScript.cs
@@ -21,8 +21,8 @@ public class LogicScript : MonoBehaviour
 
     public void RestartGame()
     {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
         Time.timeScale = 1;
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
 
     public void GameOver()
